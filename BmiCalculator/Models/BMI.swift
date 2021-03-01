b2df1760618025e8011a431af3f6de10b2ff6871
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-01 09:31:37 +0300

    The initial commit

---
+++ BmiCalculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BmiCalculator
+//
+//  Created by Nick Krasnitskiy on 05.02.2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
